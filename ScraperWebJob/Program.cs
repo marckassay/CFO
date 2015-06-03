88a555c6e24b7a0994c56a8ae333bad2db3bb20a
Marc Kassay <marckassay@gmail.com>
2015-06-03 16:38:45 -0400

    Logging exception when 'entity already exists' occurs

--- ScraperWebJob/Program.cs
+++ ScraperWebJob/Program.cs
@@ -4,22 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using System.IO;
 
 namespace WebJob
 {
     // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
     class Program
     {
-        // Please set the following connection strings in app.config for this WebJob to run:
-        // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
             var host = new JobHost();
-            // The following code will invoke a function called ManualTrigger and 
-            // pass in data (value in this case) to the function
 
-            // TODO: consider System.InvalidOperationException when entity already exists.
-            host.Call(typeof(Functions).GetMethod("ScrapeAndStoreWOD"));
+            try
+            {
+                host.Call(typeof(Functions).GetMethod("ScrapeAndStoreWOD"));
+            }
+            catch (InvalidOperationException error)
+            {
+                Program.WriteMessage(error.InnerException.Message);
+            }
+        }
+
+        static void WriteMessage(string message)
+        {
+            Console.Error.Write(message);
         }
     }
 }
